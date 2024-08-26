6a00bdf03b33478e9c2490970112febcbb479367
venkat <rishionlinetraing2@gmail.com>
2024-08-26 08:08:31 +0530

    code

---
+++ src/main/java/myapp/com/hl/HlApplication.java
@@ -0,0 +1,13 @@
+package myapp.com.hl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myapp/com/hl/HlApplicationTests.java
@@ -0,0 +1,13 @@
+package myapp.com.hl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
